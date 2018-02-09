6aba714a7dd41e33ead50d90506f88d23b6089c5
jymassol.netmessage <jymassol@netmessage.com>
2018-02-09 12:46:07 +0100

    PART 13 of Twitter clone: NewTweetScreen

--- mobile-app/src/navigations.js
+++ mobile-app/src/navigations.js
@@ -1,5 +1,6 @@
 import React, { Component }  from 'react';
 import { addNavigationHelpers, StackNavigator, TabNavigator } from 'react-navigation';
+import { Keyboard } from 'react-native';
 import { connect } from 'react-redux';
 import { FontAwesome } from '@expo/vector-icons';
 
@@ -8,9 +9,16 @@ import ExploreScreen from './screens/ExploreScreen';
 import NotificationsScreen from './screens/NotificationsScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import AuthenticationScreen from './screens/AuthenticationScreen';
+import NewTweetScreen from './screens/NewTweetScreen';
+
+
 import HeaderAvatar from './components/HeaderAvatar';
+import ButtonHeader from './components/ButtonHeader';
+
 
 import { colors } from './utils/constants';
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
 
 const TAB_ICON_SIZE = 20;
 
@@ -63,15 +71,45 @@ const Tabs = TabNavigator({
       paddingVertical: 5
     }
   }
-})
+});
+
+const NewTweetModal = StackNavigator(
+  {
+    NewTweet: {
+      screen: NewTweetScreen,
+      navigationOptions: ({ navigation }) => ({
+        headerLeft: <HeaderAvatar />,
+        headerRight: (
+          <ButtonHeader side="right" onPress={() => {
+            Keyboard.dismiss();
+            navigation.goBack(null)
+          }
+          }>
+            <EvilIcons color={colors.PRIMARY} size={25} name="close" />
+          </ButtonHeader>
+        )
+      })
+    }
+  }, {
+    headerMode: 'none'
+  }
+)
 
 const AppMainNav = StackNavigator({
   Home: {
     screen: Tabs,
-    navigationOptions: () => ({
-      headerLeft: <HeaderAvatar />
+    navigationOptions: ({ navigation }) => ({
+      headerLeft: <HeaderAvatar />,
+      headerRight: (
+        <ButtonHeader side="right" onPress={() => navigation.navigate('NewTweet')} >
+          <SimpleLineIcons color={colors.PRIMARY} size={20} name="pencil" />
+        </ButtonHeader>
+      )
     })
   },
+  NewTweet: {
+    screen: NewTweetModal
+  }
 }, {
   cardStyle: {
     backgroundColor: '#F1F6FA',
